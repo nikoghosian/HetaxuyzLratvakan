12af157139ebf1dfc004521b0df500d07d3d716f
nikoghosian <vahe20006@gmail.com>
2024-09-17 13:32:23 +0400

    test add checkaut

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -6,6 +6,7 @@ const { subDays } = require('date-fns');
 class NewsController {
   async create(req, res) {
     try {
+      const { id: userId } = req.user;
       const {
         title,
         description,
@@ -314,6 +315,7 @@ class NewsController {
 
   async editNews(req, res) {
     try {
+      const { id: userId } = req.user;
       const { id } = req.params;
       const {
         title,
@@ -465,6 +467,7 @@ class NewsController {
   }
   async deleteNews(req, res) {
     try {
+      const { id: userId } = req.user;
       const { id } = req.params;
       await NewsDto.destroy({
         where: {
@@ -581,6 +584,7 @@ class NewsController {
   }
   async slider(req, res) {
     try {
+      const { id: userId } = req.user;
       const { id1, id2, id3, id4 } = req.query;
       await NewsDto.update({ onSlider: false, order: null }, { where: { onSlider: true } });
       await NewsDto.update({ onSlider: true, order: 1 }, { where: { id: id1 } });
--- router/news.router.js
+++ router/news.router.js
@@ -4,7 +4,7 @@ const checkAuth = require('../middlewares/superAdminMiddleware');
 
 const router = Router();
 
-router.post('/create', newsController.create);
+router.post('/create', checkAuth(['SUPERADMIN', 'ADMIN']), newsController.create);
 router.get('/getAll', newsController.getAll);
 router.get('/getOne/:id', newsController.getOne);
 router.get('/getToday', newsController.getTodaysNews);
@@ -12,16 +12,16 @@ router.get('/getMostViewed', newsController.getMostViewed);
 router.get('/search', newsController.search);
 router.get('/filter', newsController.filters);
 router.get('/relatesNews', newsController.relatesNews);
-router.put('/editNews/:id', newsController.editNews);
+router.put('/editNews/:id', checkAuth(['SUPERADMIN', 'ADMIN']), newsController.editNews);
 router.get('/getMostViewedByCountryId/:id', newsController.getMostViewedByCountryId);
 router.get('/getMostViewedAndRelates/:id', newsController.getMostViewedAndRelatesNews);
-router.delete('/delete/:id', newsController.deleteNews);
+router.delete('/delete/:id', checkAuth(['SUPERADMIN', 'ADMIN']), newsController.deleteNews);
 router.get('/latestNews/', newsController.getLatestNews);
 router.get('/getCategoriesAndNews/:id', newsController.getCategoriesAndNews);
 router.get('/getMostViewedVideo', newsController.getMostViewedVideo);
 router.get('/calendar', newsController.getNewsByCalendar);
 router.get('/watch', newsController.tesadaran);
-router.put('/slider', newsController.slider);
+router.put('/slider', checkAuth(['SUPERADMIN', 'ADMIN']), newsController.slider);
 router.get('/allViews', newsController.newsViews);
 
 module.exports = router;
